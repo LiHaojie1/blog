6d5c641a1d11e8657314dfdb4f24f6c46fcf071e
LiHaojie <lihaojie@lifull.com>
2018-10-31 05:56:17 +0000

    Second model added, Security failed.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 #Models in Rails use a singular name, and their corresponding database tables use a plural name. 
 class Article < ApplicationRecord
+    has_many :comments, dependent: :destroy  #destroy comments when article is destroyed.
     #With the validation now in place, when you call @article.save on an invalid article, it will return false.
     validates :title, presence: true,
                       length: { minimum: 5 }
 end
+    
